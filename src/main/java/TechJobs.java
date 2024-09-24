9c7ddfe77cb6c46cba693c9f705bd23b17adf3ae
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-09-24 11:55:49 -0400

    Completed task1: Implement printJobs()

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("*****");
+            for (Map.Entry<String, String> jobInfo : job.entrySet()) {
+                System.out.println(jobInfo.getKey() + ": " + jobInfo.getValue());
+            }
+            System.out.println("*****\n");
+        }
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+            return;
+        }
     }
 }
